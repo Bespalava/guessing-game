217a9bbb23cfe7c123e8ed576276342daab8ae50
ybespalava <ybespalava@gmail.com>
2021-07-12 08:46:30 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.max = max;
+      this.min = min;
     }
-
+  
     guess() {
-
+        this.solution = Math.round((this.min + this.max) / 2);
+        return this.solution;
     }
-
+  
     lower() {
-
+      this.max = this.solution;
     }
-
+  
     greater() {
-
+      this.min = this.solution;
     }
-}
+  }
+  
 
 module.exports = GuessingGame;
